a9e48741631bdfa9b33746ed4ea312c00d103f06
Volt17777 <finnbauerkraft@gmail.com>
2025-12-10 10:35:30 +0100

    i commit now, good luck everybody else

--- Bowling.sql
+++ Bowling.sql
@@ -8,8 +8,8 @@ CREATE TABLE turn_ergebnisse (
     lane_id INT NOT NULL,
     player_id INT NOT NULL,
     frame INT NOT NULL CHECK (turn_nr BETWEEN 1 AND 10),
-    throw1 INT NOT NULL CHECK (wurf1 BETWEEN 0 AND 10),
-    throw2 INT NOT NULL CHECK (wurf2 BETWEEN 0 AND 10),
+    throw1 INT NOT NULL CHECK (throw1 BETWEEN 0 AND 10),
+    throw2 INT NOT NULL CHECK (throw2 BETWEEN 0 AND 10),
     frame_total INT NOT NULL,
     datum DATETIME DEFAULT CURRENT_TIMESTAMP,
 );
